7bc13efbe49eb7b49cdfba107879b367b3d75018
Majid Motallebikashani <chakmeshma@gmail.com>
2023-02-15 23:20:34 +0330

    Good working

--- relay.py
+++ relay.py
@@ -5,13 +5,18 @@ NET_MSG_SIZE = 1024
 
 running = 1
 
+relay_local_bind_address = '0.0.0.0'
+relay_local_bind_port = 5633
+relay_remote_address = '83.170.219.86'
+relay_remote_port = 8081
+
 
 def createNewListenSocket(hostname, port):
     s = None
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((hostname, port))
-    s.listen(0)
+    s.listen()
 
     return s
 
@@ -25,40 +30,37 @@ def createNewConnectSocket(hostname, port):
     return s
 
 
-def handleEchoServer(relayacceptedsocket, tunnelhost, tunnelport):
+def handleRelay(relayacceptedsocket, tunnelhost, tunnelport):
     tunnelsocket = None
 
     try:
         global running
 
         tunnelsocket = createNewConnectSocket(tunnelhost, tunnelport)
+        print(f'New connection made to {tunnelsocket.getpeername()}')
 
-        tunnelsocket.setblocking(False)
         relayacceptedsocket.setblocking(False)
+        tunnelsocket.setblocking(False)
 
         while running:
             try:
                 relay_data = relayacceptedsocket.recv(NET_MSG_SIZE)
-                # if relay_data:
-                tunnelsocket.send(relay_data)
+                if relay_data:
+                    tunnelsocket.send(relay_data)
             except socket.error as e:
                 if e.args[0] != socket.EAGAIN and e.args[0] != socket.EWOULDBLOCK:
                     raise e
 
             try:
                 tunnel_data = tunnelsocket.recv(NET_MSG_SIZE)
-                # if tunnel_data:
-                relayacceptedsocket.send(tunnel_data)
+                if tunnel_data:
+                    relayacceptedsocket.send(tunnel_data)
             except socket.error as e:
                 if e.args[0] != socket.EAGAIN and e.args[0] != socket.EWOULDBLOCK:
                     raise e
-
-        if tunnelsocket:
-            tunnelsocket.close()
-        if relayacceptedsocket:
-            relayacceptedsocket.close()
-        print('Closed a connection')
     except:
+        pass
+    finally:
         if tunnelsocket:
             tunnelsocket.close()
         if relayacceptedsocket:
@@ -75,16 +77,16 @@ def createRelayServer(relayhost, relayport):
         while running:
             relayacceptedsocket, relayacceptedaddr = relaysocket.accept()
 
-            print(f"Accepted new connection {relayacceptedaddr}")
+            print(f"Accepted new connection from {relayacceptedaddr}")
 
-            relay_thread = threading.Thread(target=handleEchoServer, args=(relayacceptedsocket, 'www.google.com', 443))
+            relay_thread = threading.Thread(target=handleRelay,
+                                            args=(relayacceptedsocket, relay_remote_address, relay_remote_port))
             relay_thread.start()
-
-        if relaysocket:
-            relaysocket.close()
     except:
+        pass
+    finally:
         if relaysocket:
             relaysocket.close()
 
 
-createRelayServer('0.0.0.0', 800)
+createRelayServer(relay_local_bind_address, relay_local_bind_port)
